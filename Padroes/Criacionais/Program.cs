83abffc100a99f4b60104713355a957e5cd3f65c
Ricardo Santos <rjesus@adaptworks.com.br>
2023-03-27 17:30:14 -0300

    criação do projeto

---
+++ Padroes/Criacionais/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Criacionais
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pattern em .Net");
+            Console.ReadKey();
+        }
+    }
+}
